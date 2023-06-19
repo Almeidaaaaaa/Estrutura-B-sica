1c114cff13c6187d7a011fbb201f71dd1fe6e1d7
Almeidaaaaaa <deboralmeidavieira@gmail.com>
2023-06-19 11:22:17 -0300

    Script terminado

---
+++ script.js
@@ -0,0 +1,11 @@
+const OK = document.getElementById('OK');
+const input = document.getElementById('inputc');
+
+OK.onclick = ()=>{
+
+    let tmp='';
+    tmp = input.value;
+
+    alert('O nome digitado foi:' +   tmp)
+}
+//fim do código 
